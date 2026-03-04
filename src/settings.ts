cc9f468c0365ebf6b1f91181df5b774567eac7ad
OutisNyel <155797048+OutisNyel@users.noreply.github.com>
2026-03-04 20:51:18 +0800

    ADD: Implement the setting part of the SidebarFold.

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,23 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { 
+	App, 
+	PluginSettingTab, 
+	Setting, 
+	Notice
+} from "obsidian";
+import type SidebarFold from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface SidebarFoldSettings {
+	visible_list_item_number: number;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export const DEFAULT_SETTINGS: SidebarFoldSettings = {
+	visible_list_item_number: 7
 }
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class SidebarFoldSettingTab extends PluginSettingTab {
+	plugin: SidebarFold;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SidebarFold) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -23,14 +28,29 @@ export class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Visible list item number')
+			.setDesc('Change the number of visible items in the folded list')
+			.addText((num) => {
+			  num.inputEl.type = "number";
+			  num.inputEl.inputMode = "numeric"; // 移动端数字键盘
+			  num.inputEl.min = "1";
+			  num.inputEl.max = "100";
+			  num.inputEl.step = "1";
+
+			  num
+				.setValue(String(this.plugin.settings.visible_list_item_number))
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				  const n = Number(value);
+				  if (!Number.isInteger(n) || n < 1) {
+					new Notice('Please enter an integer greater than or equal to 1.');
+					// this.plugin.settings.visible_list_item_number = DEFAULT_SETTINGS.visible_list_item_number;
+					return;
+				  }
+				  this.plugin.settings.visible_list_item_number = n;
+				  await this.plugin.saveSettings();
+				})
+					.setPlaceholder('Enter a number');
+			});
+
 	}
 }
